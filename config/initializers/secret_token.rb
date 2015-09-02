5fc4f002068a189392f2f3c342af9faa13e9e225
RohineeMataleY <rohineematale@gmail.com>
2015-09-02 18:24:24 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FeedFoundrie::Application.config.secret_key_base = 'f7994bae01c7827c4c455a56ba9f0d0b1ffa3d76a6cd46583acfe4d707c474171fdbf0ee13ce14c7a67abea74f196709712badb722fc61fc4fcc9b0d4056a512'
