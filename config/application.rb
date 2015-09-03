1efe02d7b8a84f812eaece5bf075ad4dfd8c1f4c
Sagar Patil <sagar.patil.smp@gmail.com>
2015-09-03 23:32:17 +0530

    assets

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,8 @@ module FeedFoundrie
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.action_mailer.delivery_method = :smtp
   end
 end
