f7c6792bd12ff5d1f4388166e0e08c1f28486590
Sagar Patil <sagar.patil.smp@gmail.com>
2015-09-03 11:46:25 +0530

    gems updatation

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,21 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+#require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "active_resource/railtie"
+require "sprockets/railtie"
+Bundler.require(*Rails.groups(:assets => %w(development test)))
 
 module FeedFoundrie
   class Application < Rails::Application
+    # config.generators do |g|
+    #   g.orm :mongoid
+    # end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
