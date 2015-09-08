8de79e32939468e578b670b245c0f1c270710a58
RohineeMataleY <rohineematale@gmail.com>
2015-09-08 18:23:02 +0530

    email confirmation error solved

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,5 @@ FeedFoundrie::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = {:host => "feed-foundrie.herokuapp.com"}
 end
