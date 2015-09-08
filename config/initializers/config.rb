6f759ca174662c03344194f88dd02f45d698b91e
RohineeMataleY <rohineematale@gmail.com>
2015-09-08 17:07:13 +0530

    error

---
+++ config/initializers/config.rb
@@ -0,0 +1,8 @@
+# MAILGUNCONFIG = YAML.load_file("#{Rails.root.to_s}/config/config.yml")[Rails.env]
+# EMAIL_CONFIG = YAML.load_file(Rails.root.join('config', 'config.yml'))[Rails.env]
+
+# Rails.application.config.ENV = YAML.load_file(Rails.root.join('config', 'config.yml'))[Rails.env]
+
+# ENV.update YAML.load_file('config/email_config.yml')[Rails.env]
+
+# ENV = OpenStruct.new YAML.load_file("#{Rails.root}/config/email_config.yml")[Rails.env]
